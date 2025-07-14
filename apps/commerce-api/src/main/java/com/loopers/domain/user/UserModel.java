04cd19041f07b440edce9adc1a93eb77728fb98e
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-07-14 11:58:11 +0900

    refactor: 계정 생성시 아이디 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/user/UserModel.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/UserModel.java
@@ -22,6 +22,10 @@ public class UserModel extends BaseEntity {
   public UserModel() {
   }
 
+  public UserModel(String userId) {
+    this.userId = userId;
+  }
+
   public String getUserId() {
     return userId;
   }
