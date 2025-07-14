7b6f4716e32dc6a961a50ac808a9cd8f13f8f5e5
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-07-14 15:46:15 +0900

    fix: 생년월일을 문자열로 받기 위해 컴파일 에러 수정

--- apps/commerce-api/src/main/java/com/loopers/application/user/UserInfo.java
+++ apps/commerce-api/src/main/java/com/loopers/application/user/UserInfo.java
@@ -1,15 +1,14 @@
 package com.loopers.application.user;
 
 import com.loopers.domain.user.UserModel;
-import java.time.LocalDate;
 
-public record UserInfo(String userId, String email, LocalDate birthday) {
+public record UserInfo(String userId, String email, String birthday) {
 
   public static UserInfo from(UserModel model) {
     return new UserInfo(
         model.getUserId(),
         model.getEmail(),
-        model.getBirthday()
+        model.getBirthday().toString()
     );
   }
 
