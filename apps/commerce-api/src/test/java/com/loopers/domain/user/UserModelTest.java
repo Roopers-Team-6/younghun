cab187698c3eb76b2d5ede6c1332b2da1806c0b3
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-07-14 11:58:36 +0900

    test: 회원가입시 ID가 영문 10자 이내일시 정상 생성되어진다.

---
+++ apps/commerce-api/src/test/java/com/loopers/domain/user/UserModelTest.java
@@ -0,0 +1,31 @@
+package com.loopers.domain.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class UserModelTest {
+
+
+  @DisplayName("회원가입을 했을때, ")
+  @Nested
+  class Create {
+
+    @DisplayName("ID가 영문 10자 이내, 정상적으로 생성된다.")
+    @Test
+    void createUser_alphabeticIdUnderMaxLength_createsUserSuccessfully() {
+      // arrange
+      String userId = "useruserId";
+      // act
+      UserModel userModel = new UserModel(userId);
+      // assert
+      assertThat(userModel.getUserId().length()).isLessThanOrEqualTo(10);
+    }
+    
+  }
+}
