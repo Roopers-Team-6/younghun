a1f7d9392991390abfcddac9c1933354b881568c
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-07-15 00:56:05 +0900

    test: 이미 가입된 아이디로 회원가입시 실패.

--- apps/commerce-api/src/test/java/com/loopers/domain/user/UserServiceIntegrationTest.java
+++ apps/commerce-api/src/test/java/com/loopers/domain/user/UserServiceIntegrationTest.java
@@ -1,6 +1,11 @@
 package com.loopers.domain.user;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThrows;
+
 import com.loopers.infrastructure.user.UserJpaRepository;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import com.loopers.utils.DatabaseCleanUp;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -41,5 +46,19 @@ public class UserServiceIntegrationTest {
       // assert
       Mockito.verify(userJpaRepository, Mockito.times(1)).save(userModel);
     }
+
+
+    @DisplayName("이미 가입된 ID 로 회원가입 시도 시, 실패한다.")
+    @Test
+    void throwsBadRequestException_saveUser_whenAlreadyId() {
+      // arrange
+      UserModel userModel = new UserModel("userId", "test@test.com", "2020-01-01");
+      userService.createUser(userModel);
+      // act
+      CoreException result = assertThrows(CoreException.class, () -> userService.createUser(userModel));
+      // assert
+      assertThat(result.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+    }
+
   }
 }
