e2b957191a5da27487e8204e69e1f63e7e0dfb2c
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-07-17 00:31:05 +0900

    fix: 요구사항에 맞게 API 수정 (#3)

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1Controller.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1Controller.java
@@ -8,9 +8,9 @@ import com.loopers.interfaces.api.user.UserV1Dto.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,8 +28,8 @@ public class UserV1Controller implements UserV1ApiSpec {
   }
 
   @Override
-  @GetMapping("/{userId}")
-  public ApiResponse<UserV1Dto.UserGetResponse> getUser(@PathVariable String userId) {
+  @GetMapping("/me")
+  public ApiResponse<UserV1Dto.UserGetResponse> getUser(@RequestHeader(name = "X-User-Id") String userId) {
     UserInfo user = userFacade.getUser(userId);
     return ApiResponse.success(UserGetResponse.from(user));
   }
